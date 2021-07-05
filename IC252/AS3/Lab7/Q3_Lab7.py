4ba61884cd79e55cd54207bf96114cdf43d92176
Aryan apte <b20186@students.iitmandi.ac.in>
2021-07-05 20:15:12 +0530

    Changed numpy to self-made functions

--- IC252/AS3/Lab7/Q3_Lab7.py
+++ IC252/AS3/Lab7/Q3_Lab7.py
@@ -1,6 +1,5 @@
 from math import sqrt
 import pandas as pd
-import numpy as np
 
 def Relation(z):
     x = float(z)
@@ -16,34 +15,49 @@ def Relation(z):
         return "Very Strong"
     elif x == 1:
         return "Perfect"
-    
+
+def mean(x):
+    add = 0
+    for i in x:
+        add += i
+    return add/len(x)
+
+def Cov(x, y):
+    add = 0
+    for i in range(len(x)):
+        add += (x[i] - mean(x))*(y[i] - mean(y))
+    return add/(len(x) - 1)
+
+def Corr(x, y):
+    return Cov(x, y)/sqrt(Cov(x, x)*Cov(y, y))
+
 data = pd.read_csv('BNG-Device.csv')
 activeSeries = pd.Series(data['Active-Count'])
 cpuSeries = pd.Series(data['CPU-Utilization'])
+avgTempSeries = pd.Series(data['Average-Temperature'])
+memorySeries = pd.Series(data['Total-Memory-Usage'])
+
 # i part
-y = "{0:.4f}".format(np.corrcoef(activeSeries, cpuSeries)[0, 1])
+y = "{0:.4f}".format(Corr(activeSeries, cpuSeries))
 print("Correlation between Active-Count and CPU-Utilization:", 
 y, "| Relation:",Relation(y))
 
 #ii part
-avgTempSeries = pd.Series(data['Average-Temperature'])
-memorySeries = pd.Series(data['Total-Memory-Usage'])
-y = "{0:.4f}".format(np.corrcoef(cpuSeries, memorySeries.fillna(0))[0, 1])
+y = "{0:.4f}".format(Corr(cpuSeries, memorySeries.fillna(0)))
 print("Correlation between CPU-Utilization and Total-Memory-Usage:", 
 y, "| Relation:", Relation(y))
 
 # iii part
-y = "{0:.4f}".format(np.corrcoef(cpuSeries, avgTempSeries)[0, 1])
-avgTempSeries = pd.Series(data['Average-Temperature'])
+y = "{0:.4f}".format(Corr(cpuSeries, avgTempSeries))
 print("Correlation between CPU-Utilization and Average-Temperature:", 
 y, "| Relation:", Relation(y))
 
 # iv part
-y = "{0:.4f}".format(np.corrcoef(activeSeries, avgTempSeries)[0, 1])
+y = "{0:.4f}".format(Corr(activeSeries, avgTempSeries))
 print("Correlation between Active-Count and Average-Temperature:", 
 y, "| Relation:", Relation(y))
 
 # v part
-y = "{0:.4f}".format(np.corrcoef(memorySeries.fillna(0), avgTempSeries)[0, 1])
+y = "{0:.4f}".format(Corr(memorySeries.fillna(0), avgTempSeries))
 print("Correlation between CPU-Utilization and Average-Temperature:", 
 y, "| Relation:", Relation(y))
